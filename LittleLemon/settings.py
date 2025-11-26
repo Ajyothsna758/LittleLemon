b4ed37e1f34f8d7c216e4bfaa0f09e67663f05c5
Ajyothsna758 <jyoece758@gmail.com>
2025-11-26 02:28:21 +0000

    setup mysql conncetion

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -74,9 +74,18 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+     
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon', #database name
+        'HOST': '127.0.0.1',
+        'PORT':'3306',
+        'USER':'root',
+        'PASSWORD':'mypassword',
     }
 }
 
